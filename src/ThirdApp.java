0762097f9a46c62b8e270269aa153a24dae3576d
randomize <randomize16@gmail.com>
2021-05-24 20:58:32 +0300

    Added new method to master branch

--- src/ThirdApp.java
+++ src/ThirdApp.java
@@ -2,5 +2,10 @@ public class ThirdApp {
     public static void main(String[] args) {
         System.out.println("Hello Java");
         System.out.println("Second line");
+        System.out.println(sum(1 , 3));
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
     }
 }
