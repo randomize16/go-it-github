bbd715435cd5c4a4dbc61a89c0c2deb8cf4aa3a4
randomize <randomize16@gmail.com>
2021-05-24 21:06:07 +0300

    Added new feature with User

--- src/ThirdApp.java
+++ src/ThirdApp.java
@@ -1,10 +1,7 @@
 public class ThirdApp {
     public static void main(String[] args) {
-        System.out.println("Hello Java");
-        System.out.println("Second line");
-        printSomeText("GOIT");
-        System.out.println(sum(1 , 3));
-
+        User currentUser = new User();
+        currentUser.printUserName();
     }
 
     public static void printSomeText(String text) {
