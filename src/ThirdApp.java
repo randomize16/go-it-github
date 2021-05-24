3539f18b612272b698ef4b291466daedd38eca93
randomize <randomize16@gmail.com>
2021-05-24 20:55:59 +0300

    Added new feature with method printSomeText.

--- src/ThirdApp.java
+++ src/ThirdApp.java
@@ -2,5 +2,10 @@ public class ThirdApp {
     public static void main(String[] args) {
         System.out.println("Hello Java");
         System.out.println("Second line");
+        printSomeText("GOIT");
+    }
+
+    public static void printSomeText(String text) {
+        System.out.println(text);
     }
 }
